73f2f7ed871cfe469a639791cded9bcc47a7ba83
ThomasDlv0 <thomas.delville000@icloud.com>
2025-10-03 14:15:51 +0200

    js

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const boxPopup = document.querySelector(".popup")
+const btnPopup = document.querySelector("#btn-js");
+btnPopup.addEventListener("click", () => {
+    boxPopup.style.display = "none";
+})
