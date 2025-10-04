08ec8d48fddca58075dbc0d330810ad4b3557b94
ThomasDlv0 <thomas.delville000@icloud.com>
2025-10-04 10:27:04 +0200

    oublie ouverture js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 const boxPopup = document.querySelector(".popup")
 const btnPopup = document.querySelector("#btn-js");
+const btnDownloead = document.querySelector(".btn-cta-download");
 btnPopup.addEventListener("click", () => {
     boxPopup.style.display = "none";
+})
+
+btnDownloead.addEventListener("click", () => {
+    boxPopup.style.display = "block";
 })
